d007aad629170367805898427507f4de8f77fe6f
Neo-Zenith <juin.lee@outlook.com>
2024-01-21 01:02:26 +0800

    removed unnecessary buttons

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -53,15 +53,6 @@ export default function Navbar() {
       }}
     >
       <Toolbar>
-        <IconButton
-          size="large"
-          edge="start"
-          color="inherit"
-          aria-label="menu"
-          sx={{ mr: 2 }}
-        >
-          <MenuIcon />
-        </IconButton>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           APP NAME
         </Typography>
