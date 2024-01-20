c30bc489c90396f45d7a166f493b00f5bfa863b3
LimIvan336 <71662324+LimIvan336@users.noreply.github.com>
2024-01-20 17:57:46 +0800

    fixing sockets

--- client/src/components/Output/Output.jsx
+++ client/src/components/Output/Output.jsx
@@ -5,19 +5,25 @@ import { io } from "socket.io-client";
 export default function Output() {
   const [context, setContext] = useState(""); // Assuming context is a string
 
-  useEffect(() => {
-    // Establish WebSocket connection
-    const socket = io("http://127.0.0.1:5000");
+  // Establish WebSocket connection
+  const socket = io("http://127.0.0.1:5000");
 
-    // client-side
-    socket.on("connect", () => {
-      console.log(socket.id);
-    });
+  // client-side
+  socket.on("connect", () => {
+    console.log(socket.id);
+  });
 
-    socket.on("disconnect", () => {
-      console.log(socket.id); // undefined
-    });
-  }, []); // Run this effect only once when the component mounts
+  socket.on("disconnect", () => {
+    console.log(socket.id); // undefined
+  });
+
+  socket.on("new-output", (data) => {
+    console.log("getting new output");
+    console.log(data);
+    // setContext((prev) => prev + data);
+  });
+
+  useEffect(() => {}, []); // Run this effect only once when the component mounts
 
   return <OutputWindow context={context} />;
 }
