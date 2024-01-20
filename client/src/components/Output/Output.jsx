060b0d62874915224941e54948fcbec8c1b998db
LimIvan336 <71662324+LimIvan336@users.noreply.github.com>
2024-01-21 02:03:54 +0800

    stream output to client

--- client/src/components/Output/Output.jsx
+++ client/src/components/Output/Output.jsx
@@ -2,35 +2,44 @@ import { useEffect, useState } from "react";
 import OutputWindow from "../OutputWindow/OutputWindow";
 import { io } from "socket.io-client";
 import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 
 export default function Output() {
-	// const [context, setContext] = useState({
-	// 	code: "",
-	// 	language: "",
-	// 	showLineNumbers: true,
-	// });
-	const codeOutput = useSelector((state) => state.codeOutput);
-	console.log("Code output:", codeOutput);
+  // const [context, setContext] = useState({
+  // 	code: "",
+  // 	language: "",
+  // 	showLineNumbers: true,
+  // });
+  const codeOutput = useSelector((state) => state.codeOutput);
+  const dispatch = useDispatch();
+  console.log("Code output:", codeOutput);
 
-	// Establish WebSocket connection
-	// const socket = io("http://127.0.0.1:5000");
+  // Establish WebSocket connection
+  const socket = io("http://127.0.0.1:5000");
 
-	// // client-side
-	// socket.on("connect", () => {
-	//   console.log(socket.id);
-	// });
+  // // client-side
+  socket.on("connect", () => {
+    console.log(socket.id);
+  });
 
-	// socket.on("disconnect", () => {
-	//   console.log(socket.id); // undefined
-	// });
+  // socket.on("disconnect", () => {
+  //   console.log(socket.id); // undefined
+  // });
 
-	// socket.on("new-output", (data) => {
-	//   console.log("getting new output");
-	//   console.log(data);
-	//   // setContext((prev) => prev + data);
-	// });
+  socket.on("new-openai-output", (data) => {
+    console.log("getting new output");
+    // setContext((prev) => prev + data);
+    dispatch({
+      type: "DISPLAY_CODE_OUTPUT",
+      payload: {
+        code: data,
+        language: "c",
+      },
+    });
+    console.log(data);
+  });
 
-	// useEffect(() => {}, []); // Run this effect only once when the component mounts
+  // useEffect(() => {}, []); // Run this effect only once when the component mounts
 
-	return <OutputWindow context={codeOutput} />;
+  return <OutputWindow context={codeOutput} />;
 }
