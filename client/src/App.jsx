8e94c853e8829d12934db2af02d1afd6dcecebb3
Neo-Zenith <juin.lee@outlook.com>
2024-01-20 22:23:05 +0800

    signup page setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import QueryPage from "./pages/QueryPage";
+import SignupPage from "./pages/SignupPage";
 
 function App() {
-	return (
-		<>
-			<Router>
-				<Routes>
-					<Route path="/" element={<LandingPage />} />
-					<Route path="/query" element={<QueryPage />} />
-				</Routes>
-			</Router>
-		</>
-	);
+    return (
+        <>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<LandingPage />} />
+                    <Route path="/query" element={<QueryPage />} />
+                    <Route path="/signup" element={<SignupPage />} />
+                </Routes>
+            </Router>
+        </>
+    );
 }
 
 export default App;
---
+++ client/src/components/Forms/SignupForm.jsx
@@ -0,0 +1,94 @@
+import { Box, Button, Typography } from "@mui/material";
+import CustomTextField from "../TextField/TextField";
+import { darken } from "@mui/material/styles";
+
+export default function SignupForm() {
+    return (
+        <>
+            <Box
+                sx={{
+                    width: "50%",
+                    minWidth: "25rem",
+                    marginLeft: "max(10%, 2rem)",
+                    marginRight: "max(10%, 2rem)",
+                    display: "flex",
+                    flexDirection: "column",
+                    rowGap: "2.5rem",
+                }}
+            >
+                <Box
+                    sx={{
+                        display: "flex",
+                        flexDirection: "column",
+                        rowGap: "0.4rem",
+                    }}
+                >
+                    <Typography
+                        variant="h5"
+                        sx={{
+                            textAlign: "center",
+                            fontFamily: "Roboto",
+                            fontWeight: 600,
+                            color: "var(--color-accent)",
+                        }}
+                    >
+                        SIGN UP
+                    </Typography>
+                    <Typography
+                        variant="subtitle1"
+                        sx={{
+                            textAlign: "center",
+                            fontFamily: "Roboto",
+                            fontWeight: 300,
+                            color: "var(--color-accent)",
+                        }}
+                    >
+                        Already have an account?&nbsp;&nbsp;
+                        <a href="" style={{ color: "var(--color-tertiary)" }}>
+                            Login now
+                        </a>
+                        .
+                    </Typography>
+                </Box>
+                <Box
+                    sx={{
+                        display: "flex",
+                        flexDirection: "column",
+                        rowGap: "1rem",
+                    }}
+                >
+                    <CustomTextField label={"Email address"} />
+                    <CustomTextField label={"Name"} />
+                    <CustomTextField label={"Password"} type="password" />
+                    <CustomTextField
+                        label={"Confirm Password"}
+                        type="password"
+                    />
+                </Box>
+                <Box sx={{ marginLeft: "auto" }}>
+                    <Button
+                        variant="contained"
+                        disableElevation
+                        sx={{
+                            backgroundColor: "var(--color-secondary)",
+                            "&:hover": {
+                                backgroundColor: darken("#0C7489", 0.1), // Adjust the factor (0.1) as needed
+                            },
+                            color: "var(--color-secondary)",
+                            padding: "0.5rem 1.5rem",
+                        }}
+                    >
+                        <Typography
+                            sx={{
+                                color: "var(--color-accent)",
+                                fontSize: "1rem",
+                            }}
+                        >
+                            SIGN UP
+                        </Typography>
+                    </Button>
+                </Box>
+            </Box>
+        </>
+    );
+}
