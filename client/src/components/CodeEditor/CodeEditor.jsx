0628bbe7c9c1ce0c76443eb8724977ab8e08df2f
rowentey <kaiseong02@gmail.com>
2024-01-21 00:33:30 +0800

    Added fetch to prompt input

--- client/src/components/CodeEditor/CodeEditor.jsx
+++ client/src/components/CodeEditor/CodeEditor.jsx
@@ -6,135 +6,136 @@ import "./COdeEditor.css";
 import QueryButtons from "../QueryButtons/QueryButtons";
 
 const languageCommentsMap = new Map([
-	["javascript", "// Paste/Enter your code here"],
-	["python", "# Paste/Enter your code here"],
-	["java", "// Paste/Enter your code here"],
-	["html", "<!-- Paste/Enter your code here -->"],
-	["css", "/* Paste/Enter your code here */"],
-	["ruby", "# Paste/Enter your code here"],
-	// Add more languages as needed
+  ["javascript", "// Paste/Enter your code here"],
+  ["python", "# Paste/Enter your code here"],
+  ["java", "// Paste/Enter your code here"],
+  ["html", "<!-- Paste/Enter your code here -->"],
+  ["css", "/* Paste/Enter your code here */"],
+  ["ruby", "# Paste/Enter your code here"],
+  // Add more languages as needed
 ]);
 
 export default function CodeEditor({ defaultCode }) {
-	const editorRef = useRef(null);
-	const dispatch = useDispatch();
-	const [language, setLanguage] = useState("Python");
-	const [code, setCode] = useState(defaultCode);
+  const editorRef = useRef(null);
+  const dispatch = useDispatch();
+  const [language, setLanguage] = useState("Python");
+  const [code, setCode] = useState(defaultCode);
 
-	function handleEditorDidMount(editor, monaco) {
-		editorRef.current = editor;
-		console.log("onMount: the editor instance:", editor);
-		console.log("onMount: the monaco instance:", monaco);
-	}
+  function handleEditorDidMount(editor, monaco) {
+    editorRef.current = editor;
+    console.log("onMount: the editor instance:", editor);
+    console.log("onMount: the monaco instance:", monaco);
+  }
 
-	function handleEditorWillMount(monaco) {
-		console.log("beforeMount: the monaco instance:", monaco);
-	}
+  function handleEditorWillMount(monaco) {
+    console.log("beforeMount: the monaco instance:", monaco);
+  }
 
-	function handleEditorValidation(markers) {
-		// model markers
-		markers.forEach((marker) => console.log("onValidate:", marker.message));
-	}
+  function handleEditorValidation(markers) {
+    // model markers
+    markers.forEach((marker) => console.log("onValidate:", marker.message));
+  }
 
-	function removeWordFromStart(inputString, targetWord) {
-		if (inputString.startsWith(targetWord)) {
-			return inputString.slice(targetWord.length).trim();
-		}
-		return inputString;
-	}
+  function removeWordFromStart(inputString, targetWord) {
+    if (inputString.startsWith(targetWord)) {
+      return inputString.slice(targetWord.length).trim();
+    }
+    return inputString;
+  }
 
-	function sanitizeInput(inputString) {
-		const cleanedString = removeWordFromStart(
-			inputString,
-			languageCommentsMap.get(language)
-		);
-		// Using the replace method with a regular expression to replace all occurrences
-		const sanitizedString = cleanedString.replace(/'/g, '"');
-		return sanitizedString;
-	}
+  function sanitizeInput(inputString) {
+    const cleanedString = removeWordFromStart(
+      inputString,
+      languageCommentsMap.get(language)
+    );
+    // Using the replace method with a regular expression to replace all occurrences
+    const sanitizedString = cleanedString.replace(/'/g, '"');
+    return sanitizedString;
+  }
 
-	// handle when user clicks on "Generate" button
-	function handleGenerate() {
-		let content = sanitizeInput(code);
+  // handle when user clicks on "Generate" button
+  function handleGenerate() {
+    let content = sanitizeInput(code);
 
-		const url = "http://localhost:5000/openai";
-		const payload = {
-			language: language,
-			content: content,
-		};
-		console.log("Payload:", payload);
+    const url = "http://localhost:5000/openai";
+    const payload = {
+      language: language,
+      content: content,
+      type: "code",
+    };
+    console.log("Payload:", payload);
 
-		dispatch({ type: "SET_LOADING", payload: true });
-		fetch(url, {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify(payload), // Convert the data to JSON format
-		})
-			.then((response) => {
-				if (!response.ok) {
-					throw new Error(`HTTP error! Status: ${response.status}`);
-				}
-				return response.json(); // Parse the response body as JSON
-			})
-			.then((data) => {
-				console.log("Query success:", data);
-				dispatch({
-					type: "DISPLAY_CODE_OUTPUT",
-					payload: {
-						code: data.response.code,
-						language: "c",
-					},
-				});
-				dispatch({ type: "SET_LOADING", payload: false });
-			})
-			.catch((error) => {
-				console.error("Error fetching response:", error);
-				dispatch({ type: "SET_LOADING", payload: false });
-			});
-	}
+    dispatch({ type: "SET_LOADING", payload: true });
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload), // Convert the data to JSON format
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json(); // Parse the response body as JSON
+      })
+      .then((data) => {
+        console.log("Query success:", data);
+        dispatch({
+          type: "DISPLAY_CODE_OUTPUT",
+          payload: {
+            code: data.response.code,
+            language: "c",
+          },
+        });
+        dispatch({ type: "SET_LOADING", payload: false });
+      })
+      .catch((error) => {
+        console.error("Error fetching response:", error);
+        dispatch({ type: "SET_LOADING", payload: false });
+      });
+  }
 
-	function handleDropdownClick(choice) {
-		console.log(`Clicked on dropdown:`, choice);
-		setLanguage(choice);
-		setCode(languageCommentsMap.get(choice));
-	}
+  function handleDropdownClick(choice) {
+    console.log(`Clicked on dropdown:`, choice);
+    setLanguage(choice);
+    setCode(languageCommentsMap.get(choice));
+  }
 
-	return (
-		<>
-			<div style={{ marginBottom: "1rem" }}>
-				<MenuDropdown
-					title={language}
-					replaceTitle={true}
-					items={[
-						{
-							label: "JavaScript",
-							action: () => handleDropdownClick("javascript"),
-						},
-						{
-							label: "Python",
-							action: () => handleDropdownClick("python"),
-						},
-					]}
-				/>
-			</div>
+  return (
+    <>
+      <div style={{ marginBottom: "1rem" }}>
+        <MenuDropdown
+          title={language}
+          replaceTitle={true}
+          items={[
+            {
+              label: "JavaScript",
+              action: () => handleDropdownClick("javascript"),
+            },
+            {
+              label: "Python",
+              action: () => handleDropdownClick("python"),
+            },
+          ]}
+        />
+      </div>
 
-			<Editor
-				height="65vh"
-				theme="vs-dark"
-				className="container"
-				language={language}
-				defaultLanguage="python"
-				defaultValue={defaultCode}
-				value={code}
-				onChange={(v, e) => setCode(v)}
-				onMount={handleEditorDidMount}
-				beforeMount={handleEditorWillMount}
-				onValidate={handleEditorValidation}
-			/>
+      <Editor
+        height="65vh"
+        theme="vs-dark"
+        className="container"
+        language={language}
+        defaultLanguage="python"
+        defaultValue={defaultCode}
+        value={code}
+        onChange={(v, e) => setCode(v)}
+        onMount={handleEditorDidMount}
+        beforeMount={handleEditorWillMount}
+        onValidate={handleEditorValidation}
+      />
 
-			<QueryButtons handleClick={handleGenerate}></QueryButtons>
-		</>
-	);
+      <QueryButtons handleClick={handleGenerate}></QueryButtons>
+    </>
+  );
 }
--- client/src/components/PromptInput/PromptInput.jsx
+++ client/src/components/PromptInput/PromptInput.jsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import { Input } from "@mui/base/Input";
 import "./PromptInput.css";
-import TextField from "@mui/material/TextField";
 import { Typography } from "@mui/material";
 import QueryButtons from "../QueryButtons/QueryButtons";
+import { useDispatch } from "react-redux";
 
 export default function PromptInput() {
-	const [inputValue, setInputValue] = useState("");
-	const [error, setError] = useState("");
+  const [inputValue, setInputValue] = useState("");
+  const [error, setError] = useState("");
+  const dispatch = useDispatch();
 
   const handleInputChange = (e) => {
     setInputValue(e.target.value);
@@ -22,33 +23,70 @@ export default function PromptInput() {
     }
 
     // Perform further validation or submit the form
+    const url = "http://localhost:5000/openai";
+    const payload = {
+      language: "",
+      content: inputValue.trim(),
+      type: "exp",
+    };
+    console.log("Payload:", payload);
+
+    dispatch({ type: "SET_LOADING", payload: true });
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload), // Convert the data to JSON format
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json(); // Parse the response body as JSON
+      })
+      .then((data) => {
+        console.log("Query success:", data);
+        dispatch({
+          type: "DISPLAY_CODE_OUTPUT",
+          payload: {
+            code: data.response.code,
+            language: "c",
+          },
+        });
+        dispatch({ type: "SET_LOADING", payload: false });
+      })
+      .catch((error) => {
+        console.error("Error fetching response:", error);
+        dispatch({ type: "SET_LOADING", payload: false });
+      });
 
     // Reset the input value and error state
     setInputValue("");
     setError("");
   };
 
-	return (
-		<div
-			style={{
-				display: "flex",
-				flexDirection: "column",
-				gap: "0.5rem",
-				width: "100%",
-				height: "100%",
-				paddingLeft: "1rem",
-				justifyContent: "center",
-			}}
-		>
-			<Typography sx={{ fontSize: "3rem", fontWeight: 700 }}>
-				Text Input
-			</Typography>
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "0.5rem",
+        width: "100%",
+        height: "100%",
+        paddingLeft: "1rem",
+        justifyContent: "center",
+      }}
+    >
+      <Typography sx={{ fontSize: "3rem", fontWeight: 700 }}>
+        Text Input
+      </Typography>
 
-			<Input
-				slotProps={{ input: { className: "prompt-input" } }}
-				placeholder="Enter your prompt here…"
-			/>
-			<QueryButtons />
-		</div>
-	);
+      <Input
+        slotProps={{ input: { className: "prompt-input" } }}
+        placeholder="Enter your prompt here…"
+      />
+      <QueryButtons />
+    </div>
+  );
 }
