92b4e5ab1c81e2e4853498cf11e0f721cd8855d7
rowentey <kaiseong02@gmail.com>
2024-01-20 17:43:39 +0800

    Added styling to CodeEditor and language changing logic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,27 @@
 import OutputWindow from "./components/OutputWindow/OutputWindow";
-import CodeEditor from "./components/CodeEditor";
+import CodeEditor from "./components/CodeEditor/CodeEditor";
+import { Grid } from "@mui/material";
 
 function App() {
-    // testing OutputWindow.jsx on main page
-    return (
-        <>
+	return (
+		<>
 			<h1>Monaco Editor Example</h1>
-			<CodeEditor
-				defaultCode={"/// some long ass comment so i can see whats happening"}
-			/>
-            <OutputWindow
-                context={{
-                    code: "const add = 1 + 2;\nvar i = add;",
-                    language: "javascript",
-                    showLineNumbers: true,
-                }}
-            />
-        </>
-    );
+			<Grid container spacing={2}>
+				<Grid item xs={6}>
+					<CodeEditor defaultCode={"# Paste/Enter your code here"} />
+				</Grid>
+				<Grid item xs={6}>
+					<OutputWindow
+						context={{
+							code: "const add = 1 + 2;\nvar i = add;",
+							language: "javascript",
+							showLineNumbers: true,
+						}}
+					/>
+				</Grid>
+			</Grid>
+		</>
+	);
 }
 
 export default App;
