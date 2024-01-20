bc1a89c4fc76018acdd59cb1589f95fdef8f8d12
Neo-Zenith <juin.lee@outlook.com>
2024-01-20 15:20:09 +0800

    testing outputwindow with react code block

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,18 @@
+import OutputWindow from "./components/OutputWindow/OutputWindow";
+
 function App() {
-    return <></>;
+    // testing OutputWindow.jsx on main page
+    return (
+        <>
+            <OutputWindow
+                context={{
+                    code: "const add = 1 + 2;\nvar i = add;",
+                    language: "javascript",
+                    showLineNumbers: true,
+                }}
+            />
+        </>
+    );
 }
 
 export default App;
--- client/src/components/OutputWindow/OutputWindow.jsx
+++ client/src/components/OutputWindow/OutputWindow.jsx
@@ -1,3 +1,39 @@
-export default function OutputWindow() {
-    
+import { Box, Typography } from "@mui/material";
+import { CodeBlock, CopyBlock } from "react-code-blocks";
+import "./OutputWindow.css";
+
+export default function OutputWindow({ context }) {
+    return (
+        <>
+            <Box
+                component="span"
+                sx={{
+                    display: "block",
+                    p: 1,
+                    m: 1,
+                    bgcolor: (theme) =>
+                        theme.palette.mode === "dark" ? "#101010" : "#fff",
+                    color: (theme) =>
+                        theme.palette.mode === "dark" ? "grey.300" : "grey.800",
+                    border: "1px solid",
+                    borderColor: (theme) =>
+                        theme.palette.mode === "dark" ? "grey.800" : "grey.300",
+                    borderRadius: 2,
+                    fontSize: "0.875rem",
+                }}
+            >
+                <Typography sx={{ fontSize: "1rem", fontWeight: 700 }}>
+                    Output
+                </Typography>
+                <div className="output-content">
+                    <CodeBlock
+                        text={context.code}
+                        language={context.language}
+                        showLineNumbers={context.showLineNumbers}
+                        wrapLines
+                    />
+                </div>
+            </Box>
+        </>
+    );
 }
