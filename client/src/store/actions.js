43bafcb8e311f470429872c24a096f30012330e5
Neo-Zenith <juin.lee@outlook.com>
2024-01-20 20:14:34 +0800

    added redux store for persistent state management & landing page scaffolding

---
+++ client/src/store/actions.js
@@ -0,0 +1,9 @@
+export const setUsername = (username) => ({
+    type: "SET_USERNAME",
+    payload: username,
+});
+
+export const setAuthenticated = (authenticated) => ({
+    type: "SET_AUTHENTICATED",
+    payload: authenticated,
+});
