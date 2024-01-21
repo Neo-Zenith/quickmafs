52d51433da12e3858482bcb39de06ff088e7f283
Neo-Zenith <juin.lee@outlook.com>
2024-01-21 10:49:07 +0800

    updated landing page content

--- client/src/components/HeroSection/HeroSection.jsx
+++ client/src/components/HeroSection/HeroSection.jsx
@@ -73,7 +73,8 @@ export default function HeroSection() {
             "&:hover": {
               backgroundColor: darken("#0C7489", 0.1), // Adjust the factor (0.1) as needed
             },
-            maxWidth: "max(70%, 20rem)",
+            padding: "0.5rem 1.5rem",
+            width: "fit-content",
           }}
         >
           <Typography>Try Now</Typography>
